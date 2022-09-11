7e5848b2a3e2f69a004c65b2c5ebf4c91023c202
FranciscoFrias17 <Francisco.frias17@gmail.com>
2022-09-11 16:03:23 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,22 @@ import user_routes from "./handlers/user";
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
+const port = process.env.EXPRESS_PORT;
 
 app.use(bodyParser.json());
 
 app.get("/", function (req: Request, res: Response) {
-  res.send("Hello World!");
+  res.send("Server is up and running!");
 });
 
 order_routes(app);
 product_routes(app);
 user_routes(app);
 
-app.listen(3000, function () {
-  console.log(`starting app on: ${address}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port, function () {
+    console.log(`starting app on: ${address}`);
+  });
+}
+
+export default app;
