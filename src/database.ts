01bdfda29928c9cec086c0bad0856e8c3b70d110
FranciscoFrias17 <Francisco.frias17@gmail.com>
2022-09-11 17:30:23 -0400

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -14,7 +14,7 @@ const {
 
 const client = new Pool({
   host: POSTGRES_URL,
-  database: NODE_ENV === "test" ? POSTGRES_DB : "postgres",
+  database: NODE_ENV === "test" ? POSTGRES_TEST_DB : POSTGRES_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
 });
